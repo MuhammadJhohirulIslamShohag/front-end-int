4d263621efc54ebe5dbc9507f3a6b4a44d35e146
MuhammadShohagIslam <muhammadshohagislam.software.e@gmail.com>
2026-08-08 14:56:28 +0100

    (done) project setup for image carousel.

---
+++ react/image_carousel/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ImageCarousel from "./pages/ImageCarousel/ImageCarousel";
+
+function App() {
+  return (
+    // Main application wrapper container
+    <div className="App">
+      <h1>Image Carousel</h1>
+      <ImageCarousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/image_carousel/src/pages/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,9 @@
+const ImageCarousel = () => {
+    return (
+        <div>
+            <h1>Image Carousel</h1>
+        </div>
+    );
+}
+
+export default ImageCarousel;
